615bda94dc3390e0bdf6cc8a0a177431f8dfc263
garkusha <Dfcbkmtdf1@>
2019-01-24 16:36:04 +0200

    added path to Selenium Hub

--- src/test/java/UiForMavenTest.java
+++ src/test/java/UiForMavenTest.java
@@ -2,13 +2,16 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static net.sourceforge.htmlunit.corejs.javascript.tools.SourceReader.toUrl;
 
 public class UiForMavenTest {
 
     @Test
     public void testAmazon() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new RemoteWebDriver(toUrl("http://172.19.81.145:4444/wd/hub"), DesiredCapabilities.chrome());
         driver.manage().window().maximize();
         driver.get("https://www.amazon.com");
 
@@ -24,7 +27,7 @@ public class UiForMavenTest {
 
     @Test
     public void testApple() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new RemoteWebDriver(toUrl("http://172.19.81.145:4444/wd/hub"), DesiredCapabilities.chrome());
         driver.manage().window().maximize();
         driver.get("https://pn.com.ua");
 
